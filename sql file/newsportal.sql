dc1f14b9d779b7cb2fc7116fda301dacef5330d7
Shantnu <shantnu@gmail.com>
2025-03-27 16:38:25 +0530

    update datebase sql file

--- sql file/newsportal.sql
+++ sql file/newsportal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2025 at 09:32 PM
+-- Generation Time: Mar 27, 2025 at 12:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -69,7 +69,8 @@ CREATE TABLE `tblcategory` (
 
 INSERT INTO `tblcategory` (`id`, `CategoryName`, `Description`, `PostingDate`, `UpdationDate`, `Is_Active`, `photo`, `Image`) VALUES
 (13, 'Politics', 'Covers national and international political news, government policies, elections, and diplomatic relations.', '2025-03-15 20:15:31', NULL, 1, NULL, 'uploads/category_images/360_F_383262947_guuIGJvjy6cvB0XjbprVZ48wAHWMboS7.jpg'),
-(14, 'Business & Economy', 'Focuses on market trends, stock updates, corporate news, trade policies, and economic developments.', '2025-03-15 20:21:54', NULL, 1, NULL, 'uploads/category_images/group-discussion-topics-on-business-and-economy.png');
+(14, 'Business & Economy', 'Focuses on market trends, stock updates, corporate news, trade policies, and economic developments.', '2025-03-15 20:21:54', NULL, 1, NULL, 'uploads/category_images/group-discussion-topics-on-business-and-economy.png'),
+(15, 'Sports', 'Sports news', '2025-03-27 06:59:05', NULL, 1, NULL, 'uploads/category_images/360_F_278427683_zeS9ihPAO61QhHqdU1fOaPk2UClfgPcW.jpg');
 
 -- --------------------------------------------------------
 
@@ -107,7 +108,7 @@ CREATE TABLE `tblpages` (
 --
 
 INSERT INTO `tblpages` (`id`, `PageName`, `PageTitle`, `Description`, `PostingDate`, `UpdationDate`) VALUES
-(1, 'aboutus', 'About News Portal', '<p><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">Welcome to News Portal, your one-stop destination for staying informed and up-to-date with the latest news and developments from around the world.<br></span></font><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">At&nbsp;</span></font><span style=\"color: rgb(123, 136, 152); font-family: &quot;Mercury SSm A&quot;, &quot;Mercury SSm B&quot;, Georgia, Times, &quot;Times New Roman&quot;, &quot;Microsoft YaHei New&quot;, &quot;Microsoft Yahei&quot;, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif; font-size: 26px;\">News Portal</span><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">, we understand the importance of reliable and timely information in today\'s fast-paced world. Our team of dedicated journalists and editors work tirelessly to bring you the most relevant and engaging stories from a variety of sources, ensuring that you are always well-informed on the issues that matter most to you.<br></span></font><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">Whether you\'re interested in politics, sports, entertainment, technology, or business, you\'ll find it all here on&nbsp;</span></font><span style=\"color: rgb(123, 136, 152); font-family: &quot;Mercury SSm A&quot;, &quot;Mercury SSm B&quot;, Georgia, Times, &quot;Times New Roman&quot;, &quot;Microsoft YaHei New&quot;, &quot;Microsoft Yahei&quot;, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif; font-size: 26px;\">News Portal</span><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">. Our user-friendly interface makes it easy to navigate through the latest headlines, explore in-depth articles, and discover new perspectives on the events shaping our world.<br></span></font><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">But&nbsp;</span></font><span style=\"color: rgb(123, 136, 152); font-family: &quot;Mercury SSm A&quot;, &quot;Mercury SSm B&quot;, Georgia, Times, &quot;Times New Roman&quot;, &quot;Microsoft YaHei New&quot;, &quot;Microsoft Yahei&quot;, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif; font-size: 26px;\">News Portal</span><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">&nbsp;is more than just a news website – it\'s a community of curious minds and engaged citizens who come together to discuss, debate, and share ideas. We believe in the power of journalism to inspire change and foster understanding, and we\'re committed to providing a platform where voices from all walks of life can be heard.<br></span></font><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, å¾®è½¯é›…é»‘, å®‹ä½“, SimSun, STXihei, åŽæ–‡ç»†é»‘, serif\"><span style=\"font-size: 26px;\">Join us on our mission to inform, inspire, and empower. Together, we can make a difference.</span></font></p>', '2024-01-14 18:30:00', '2024-03-03 16:56:17'),
+(1, 'aboutus', 'About News Portal', '<p style=\"color: #ffffff; font-family: \'Mercury SSm A\', \'Mercury SSm B\', Georgia, Times, \'Times New Roman\', \'Microsoft YaHei New\', \'Microsoft Yahei\', SimSun, serif; font-size: 26px;\">\r\n    Welcome to News Portal, your one-stop destination for staying informed and up-to-date with the latest news and developments from around the world.<br>\r\n\r\n    At <span style=\"color: #ffffff;\">News Portal</span>, we understand the importance of reliable and timely information in today\'s fast-paced world. Our team of dedicated journalists and editors work tirelessly to bring you the most relevant and engaging stories from a variety of sources, ensuring that you are always well-informed on the issues that matter most to you.<br>\r\n\r\n    Whether you\'re interested in politics, sports, entertainment, technology, or business, you\'ll find it all here on <span style=\"color: #ffffff;\">News Portal</span>. Our user-friendly interface makes it easy to navigate through the latest headlines, explore in-depth articles, and discover new perspectives on the events shaping our world.<br>\r\n\r\n    But <span style=\"color: #ffffff;\">News Portal</span> is more than just a news website – it\'s a community of curious minds and engaged citizens who come together to discuss, debate, and share ideas. We believe in the power of journalism to inspire change and foster understanding, and we\'re committed to providing a platform where voices from all walks of life can be heard.<br>\r\n\r\n    Join us on our mission to inform, inspire, and empower. Together, we can make a difference.\r\n</p>\r\n', '2024-01-14 18:30:00', '2025-03-27 11:02:43'),
 (2, 'contactus', 'Contact Details', '<p><br></p><p><b>Address :&nbsp; </b>New Delhi India</p><p><b>Phone Number : </b>+91 -01234567890</p><p><b>Email -id : </b>phpgurukulofficial@gmail.com</p>', '2024-01-15 18:30:00', '2024-01-31 05:44:24');
 
 -- --------------------------------------------------------
@@ -137,7 +138,7 @@ CREATE TABLE `tblposts` (
 --
 
 INSERT INTO `tblposts` (`id`, `PostTitle`, `CategoryId`, `SubCategoryId`, `PostDetails`, `PostingDate`, `UpdationDate`, `Is_Active`, `PostUrl`, `PostImage`, `viewCounter`, `postedBy`, `lastUpdatedBy`) VALUES
-(14, 'Government Announces New Economic Reform Policy', 13, 11, '<p data-start=\"256\" data-end=\"484\">The government has introduced a new economic reform policy aimed at boosting small businesses and foreign investment. The policy includes tax reductions, simplified regulations, and incentives for startups.</p><p>\r\n</p><p data-start=\"486\" data-end=\"629\">Speaking at a press conference, the Finance Minister stated, <em data-start=\"547\" data-end=\"627\">\"These reforms will strengthen our economy and create more job opportunities.\"</em></p>', '2025-03-15 20:28:20', '2025-03-15 20:30:10', 1, 'Government-Announces-New-Economic-Reform-Policy', '235dc7d6a6e04f9700a2e33cdffcc116.jpg', 1, 'admin', NULL);
+(15, ' Virat Kohli Leads RCB to Victory in IPL 2025 Opener', 15, 25, '<p data-start=\"188\" data-end=\"487\" class=\"\">Virat Kohli showcased his brilliance in the IPL 2025 opener, leading Royal Challengers Bengaluru (RCB) to a convincing 7-wicket victory over Kolkata Knight Riders (KKR). Kohli played an unbeaten knock of 59 runs and partnered with Phil Salt, who contributed 56 runs, forming a crucial 95-run stand.</p><p data-start=\"489\" data-end=\"510\" class=\"\"><strong data-start=\"489\" data-end=\"508\">Key Highlights:</strong></p><ul data-start=\"511\" data-end=\"693\">\r\n<li data-start=\"511\" data-end=\"566\" class=\"\">\r\n<p data-start=\"513\" data-end=\"566\" class=\"\">Kohli’s unbeaten 59 anchored RCB’s chase with ease.</p>\r\n</li>\r\n<li data-start=\"567\" data-end=\"628\" class=\"\">\r\n<p data-start=\"569\" data-end=\"628\" class=\"\">Phil Salt’s explosive 56 provided the necessary momentum.</p>\r\n</li>\r\n<li data-start=\"629\" data-end=\"693\" class=\"\">\r\n<p data-start=\"631\" data-end=\"693\" class=\"\">RCB’s strategy against CSK’s spinners is the next focus point.</p>\r\n</li>\r\n</ul><p data-start=\"695\" data-end=\"939\" class=\"\">Off the field, Kohli grabbed attention with his uncanny resemblance to a Turkish actor, drawing humorous comparisons from fans referencing Anushka Sharma’s film <em data-start=\"856\" data-end=\"877\">Rab Ne Bana Di Jodi</em>. Kohli remains a crowd favorite, both on and off the field.</p><p>\r\n\r\n\r\n\r\n</p><p data-start=\"941\" data-end=\"1010\" class=\"\">Stay tuned for RCB’s next encounter against Chennai Super Kings! ??</p>', '2020-03-18 07:22:28', '2021-03-18 07:45:48', 1, '-Virat-Kohli-Leads-RCB-to-Victory-in-IPL-2025-Opener', 'd3950afd92242152477b795a793c14bfjpeg', 3, 'admin', NULL);
 
 -- --------------------------------------------------------
 
@@ -150,6 +151,7 @@ CREATE TABLE `tblsubcategory` (
   `CategoryId` int(11) DEFAULT NULL,
   `Subcategory` varchar(255) DEFAULT NULL,
   `SubCatDescription` mediumtext DEFAULT NULL,
+  `Image` varchar(255) NOT NULL,
   `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
   `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
   `Is_Active` int(1) DEFAULT NULL
@@ -159,8 +161,10 @@ CREATE TABLE `tblsubcategory` (
 -- Dumping data for table `tblsubcategory`
 --
 
-INSERT INTO `tblsubcategory` (`SubCategoryId`, `CategoryId`, `Subcategory`, `SubCatDescription`, `PostingDate`, `UpdationDate`, `Is_Active`) VALUES
-(11, 13, 'Political Parties & Leaders', 'News about major political parties, their agendas, leadership changes, and key political figures.', '2025-03-15 20:23:04', NULL, 1);
+INSERT INTO `tblsubcategory` (`SubCategoryId`, `CategoryId`, `Subcategory`, `SubCatDescription`, `Image`, `PostingDate`, `UpdationDate`, `Is_Active`) VALUES
+(25, 15, 'Cricket', 'Cricket news', 'uploads/subcategory_images/2604895.jpg', '2025-03-27 07:03:23', NULL, 1),
+(26, 15, 'Kabaddi', 'Kabaddi news', 'uploads/subcategory_images/Pro-Kabaddi-2018-Bengaluru-Bulls-Riders-defenders-all-rounders.jpg', '2025-03-27 07:05:16', NULL, 1),
+(27, 15, 'Football', 'Football news', 'uploads/subcategory_images/7e186ce3aa6bfb53f2cb27e084dd0759.jpg', '2025-03-27 07:13:56', NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -224,7 +228,7 @@ ALTER TABLE `tbladmin`
 -- AUTO_INCREMENT for table `tblcategory`
 --
 ALTER TABLE `tblcategory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tblcomments`
@@ -242,13 +246,13 @@ ALTER TABLE `tblpages`
 -- AUTO_INCREMENT for table `tblposts`
 --
 ALTER TABLE `tblposts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tblsubcategory`
 --
 ALTER TABLE `tblsubcategory`
-  MODIFY `SubCategoryId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `SubCategoryId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Constraints for dumped tables
